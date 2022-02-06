3784c1a2545751105e49f9a2e4b92b1d169dc1cf
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2022-02-06 23:41:32 +1000

    Switch to notification lib version

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -26,69 +26,35 @@ function disableNotifications(): Promise<void> {
   });
 }
 
-function ensurePermission(): Promise<void> {
-  if (Notification.permission === "granted") {
-    return Promise.resolve();
-  }
-  return Notification.requestPermission().then(function (permission) {
-    // If the user accepts, let's create a notification
-    if (permission === "granted") {
-      return Promise.resolve();
-    } else {
-      return Promise.reject('Permission denied');
-    }
-  });
-}
-
 export default function Settings() {
   const timeRef = React.useRef<HTMLInputElement>();
   const [notificationsEnabled, setNotificationsEnabled] = React.useState(false);
   const enableNotificationsClick = React.useCallback(() => {
     const [chosenHours, chosenMinutes] = timeRef.current.value.split(':');
-    ensurePermission()
-    .then(() => {
-      fetch(`${endpoint}/vapid`)
-      .then(response => response.text())
-      .then(vapidKey => {
-        navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
-          disableNotifications().then(() => {
-            var options = {
-              userVisibleOnly: true,
-              applicationServerKey: vapidKey
-            };
-            serviceWorkerRegistration.pushManager.subscribe(options)
-            .then((pushSubscription) => {
-                fetch(`${endpoint}/subscribe`, {
-                  method: 'POST',
-                  body: JSON.stringify({
-                    subscription: pushSubscription,
-                    interval: `${chosenMinutes} ${chosenHours} * * *`,
-                    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
-                  }),
-                  headers: {
-                    'Content-Type': 'application/json'
-                  }
-                })
-                .then(() => {
-                  new bs5.Toast({
-                    body: notificationsEnabled ? 'Notification time updated' : 'Notifications enabled',
-                    className: 'border-0 bg-success text-white',
-                    placement: 'bottom-right',
-                    btnCloseWhite: true,
-                  }).show();
-                  setNotificationsEnabled(true);
-                })
-                .catch(() => console.log('Failed to create push subscription'));
-              }, function(error) {
-                console.log(error);
-                console.log('Failed to request notification permission')
-              }
-            );
-          });
-        });
-      });
+    (window as any).notificationScheduler({
+      interval: `${chosenMinutes} ${chosenHours} * * *`,
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    })
+    .then(_ => {
+      new bs5.Toast({
+        body: notificationsEnabled ? 'Notification time updated' : 'Notifications enabled',
+        className: 'border-0 bg-success text-white',
+        placement: 'bottom-right',
+        btnCloseWhite: true,
+      }).show();
+      setNotificationsEnabled(true);
+    })
+    .catch((error) => {
+      console.error(error);
+      new bs5.Toast({
+        header: 'Unable to set notifications',
+        body: 'Check your site notification settings',
+        className: 'border-0 bg-danger text-white',
+        placement: 'bottom-right',
+        btnCloseWhite: true,
+      }).show();
     });
-  }, []);
+  }, [notificationsEnabled]);
   const disableNotificationsClick = React.useCallback(() => {
     disableNotifications();
   }, []);
