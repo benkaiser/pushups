62da9de817620a2a5e1c64c199c0fd13ef7dcabd
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2022-05-04 22:51:37 +1000

    Add fast manual entry

--- src/screens/manual.tsx
+++ src/screens/manual.tsx
@@ -1,9 +1,10 @@
 import { getTodayGoal } from '../data/goals';
-import { recordPushups } from '../data/logs';
+import { getTodayCount, recordPushups } from '../data/logs';
 import { useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 
 export default function Manual() {
@@ -13,19 +14,31 @@ export default function Manual() {
     recordPushups(Number(pushupsDoneRef.current.value));
     navigate('/');
   }, []);
+  const onSavePushupsNum = React.useCallback((numPushups: number) => {
+    recordPushups(numPushups);
+    navigate('/');
+  }, []);
   const goal = getTodayGoal();
+  const count = getTodayCount();
+  const sizes = [5, 10, 15, 20, 25, 30];
 
   return (
     <React.Fragment>
       <h1>Enter Your Pushups!</h1>
-      <p>Manually enter the number of pushups you did outside the app today</p>
+      <p>Manually enter the number of pushups you completed:</p>
       <Form>
         <Form.Group className="mb-3" controlId="goalStart">
-          <Form.Control type="number" defaultValue={goal} ref={pushupsDoneRef} />
+          <Form.Control type="number" defaultValue={goal - count} ref={pushupsDoneRef} />
         </Form.Group>
         <Button variant="success" size='lg' type="submit" onClick={onSavePushups}>
           Save
         </Button>
+        <h3 className='mt-4'>Quick Entry</h3>
+        <Row>
+          {sizes.map(size =>
+            <Col key={size} xs='6' sm='4' className='mt-3'><div className='d-flex'><Button variant='info' className='flex-grow-1' onClick={onSavePushupsNum.bind(this, size)}>{size}</Button></div></Col>
+          )}
+        </Row>
       </Form>
     </React.Fragment>
   );
